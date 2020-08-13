521d635e27d72db4c40af22a1116c9aadaaa69da
Mananchery Panneerselvam <I29140@verisk.com>
2020-08-12 22:36:56 -0600

    Added navigation bar for the app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/todo-list";
+import EditTodo from "./components/edit-todo";
+import CreateTodo from "./components/create-todo";
+import logo from "./logo.png";
 
 
 class App extends Component{
   render(){  
+
+    const barstyle = {width:"100%"}
     return(
       <Router>
-      <div>
-        TODO MERN APP
+      <div className = "container">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style = {barstyle}>
+          <a className = "navbar-brand">
+            <img src = {logo} width = "50" height = "50" alt= "Todo App" />          
+          </a>
+          <Link to = "/" className = "navbar-brand" style = {{fontSize: "medium"}}> MERN-STACK TODO APP</Link>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+            <li class="nav-item">
+              <Link to = "/" className = "nav-link" > Todos</Link>
+            </li>
+            <li class="nav-item">
+              <Link to = "/create" className = "nav-link"> Create Todo</Link>
+            </li>
+            </ul>
+           </div>
+        </nav>
+        <Route exact path="/"  component = {TodoList}/>
+        <Route path= "/edit/:id" component = {EditTodo}/>
+        <Route path = "/create" component = {CreateTodo}/>
       </div>
-      {/* <Route path="/" exact Component = {TodoList}/>
-      <Route path= "/edit/:id" component = {EditTodo}/>
-      <Route path = "/create" component = {CreateTodo}/> */}
       </Router>
     )
-  }
+  } 
 }
 
 
