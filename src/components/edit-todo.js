cea19c42c520d51d9f43d6298dcabf09fc383754
Mananchery Panneerselvam <I29140@verisk.com>
2020-08-12 22:35:45 -0600

    Added the components for todo app

---
+++ src/components/edit-todo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class EditTodo extends Component {
+    render(){
+        return(
+            <div>
+                Edit-Todo Component
+            </div>
+        )
+    }
+}
+
+
+export default EditTodo;
---
+++ src/components/todo-list.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+
+class TodoList extends Component {
+    render(){
+        return(
+            <div>
+                Todo List Component
+            </div>
+        )
+        
+    }
+}
+
+
+
+export default TodoList;
