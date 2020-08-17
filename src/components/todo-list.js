3d01b36dd7e54814f920fc3b3823dcf5e131aa20
Mananchery Panneerselvam <mppk2009@gmail.com>
2020-08-16 21:21:04 -0600

    Initiated todo data display

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,7 +1,23 @@
 import React , {Component} from 'react';
-
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class TodoList extends Component {
+
+    constructor(props){
+        super()
+        this.state = {todos: []}
+    }
+
+    componentDidMount(){
+        axios.get("http://localhost:4000/todos")
+        .then(res => {
+            this.setState({todos: res.data})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
     render(){
         return(
             <div>
